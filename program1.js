05fbad727b4b38474bfe2eb40ce0da44cdaad8d3
Riyashukla122 <riyashukla35364@gmail.com>
2024-09-30 01:50:50 -0700

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,10 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+function isValid(s) {
+    let stack=[];
+    let map={
+        '(': ')',
+        '[':']',
+        '{':'}',
 };
-
-module.exports = { isValid };
+for(let i=0;i<s.length;i++){
+    if (s[i]==='(' || s[i]==='[' || s[i]==='{'){
+        stack.push(s[i]);
+    }
+    else{
+        if(stack.length===0 || map[stack.pop()] !==s[i]){
+            return false;
+        }
+    }
+}
+    return stack.length===0;
+}  
 
 
